ce1e2ef7af8aa94153aec02f9d04803a680c33d5
Leonardo Ferreira <leonardo.ferreira@woozaventures.com.br>
2020-06-07 19:10:25 -0300

    #3 - Implementação do middleware

---
+++ Excel.File.Service/Middleware/ExcelFileServiceMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Excel.File.Service.Middleware
+{
+    public class ExcelFileServiceMiddleware
+    {
+        readonly RequestDelegate _next;
+
+        public ExcelFileServiceMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+            => await _next.Invoke(context);
+    }
+}
---
+++ Excel.File.Service/Middleware/Extensions/ExcelFileServiceMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Excel.File.Service.Service;
+using Excel.File.Service.Service.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Excel.File.Service.Middleware.Extensions
+{
+    public static class ExcelFileServiceMiddlewareExtensions
+    {
+        public static IServiceCollection AddExcelReader(this IServiceCollection services)
+        {
+            return services.AddTransient<IImportService, ImportService>();
+        }
+    }
+}
